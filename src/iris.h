4443c962f2a5c42daf22f6c7435e4a7abcbdfd4b
Keith-Bateman <kbateman@hawk.iit.edu>
2019-10-25 19:09:22 -0500

    Added licensing information

--- src/iris.h
+++ src/iris.h
@@ -1,12 +1,30 @@
+/*
+ * Copyright (C) 2019  SCS Lab <scs-help@cs.iit.edu>, Hariharan
+ * Devarajan <hdevarajan@hawk.iit.edu>, Anthony Kougkas
+ * <akougkas@iit.edu>, Xian-He Sun <sun@iit.edu>
+ *
+ * This file is part of Iris
+ * 
+ * Iris is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 /*******************************************************************************
 * File iris.h
 *
 * Goal: Connect to the applications by including this header
 *
 * Created: December 10th, 2016  - Anthony Kougkas
- * Updated:
-* Illinois Institute of Technology - SCS Lab
-* (C) 2016
 ******************************************************************************/
 #ifndef IRIS_IRIS_H
 #define IRIS_IRIS_H
